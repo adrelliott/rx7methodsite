71915d9056f53952e7b0978f51daa83226f74d01
Al Elliott <adrelliott@gmail.com>
2023-07-31 17:30:45 +0200

    Add button element

--- resources/views/components/elements/button.blade.php
+++ resources/views/components/elements/button.blade.php
@@ -1,5 +1,15 @@
-<div>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Button
-    </button>
+@props(['icon' => false, 'href' => '/', 'label' => 'Define button text'])
+<div class="not-prose mt-2">
+    <a
+        class="bg-primary hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-base text-white font-semibold h-12 px-6 space-x-6 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400 "
+        href="{{ $href }}">
+        @if ($icon)
+            <div>
+                X
+            </div>
+        @endif
+        <div>
+            {{ $label }}
+        </div>
+    </a>
 </div>
