5d1e6276093cb1be11bad60bd1f8dded1fed4032
Al Elliott <adrelliott@gmail.com>
2023-08-01 09:53:52 +0200

    Tidy up hero

--- resources/views/components/elements/hero-centered.blade.php
+++ resources/views/components/elements/hero-centered.blade.php
@@ -1,10 +1,10 @@
 @props(['buttonText', 'buttonLink'])
-<x-layouts.section-hero>
+<x-layouts.section-hero class="">
     <h1
-        class="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center font-hubot dark:text-white">
+        class="text-slate-900 font-black text-4xl sm:text-5xl lg:text-7xl text-center font-hubot dark:text-white leading-10">
         {{ $headline ?? 'Define a headline' }}    
     </h1>
-    <p class="mt-6 text-2xl text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400">
+    <p class="mt-6 text-2xl mb-2 text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400">
         {{ $subheadline ?? 'Define a subheadline' }}
     </p>
     @isset($buttonText)
--- resources/views/components/layouts/section-hero.blade.php
+++ resources/views/components/layouts/section-hero.blade.php
@@ -1,3 +1,3 @@
-<section {{ $attributes->merge(['class' => 'px-8 py-12 sm:py-16 md:py-18 h-screen flex flex-col justify-center items-center overflow-hidden']) }}>
+<section {{ $attributes->merge(['class' => 'px-8 py-12 sm:py-16 md:py-18 mx-auto w-full md:w-2/3 h-screen flex flex-col justify-center items-center overflow-hidden']) }}>
     {{ $slot }}
 </section>
