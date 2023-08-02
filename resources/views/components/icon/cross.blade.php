119f9cdbfee75ff70d000d92c2143e813ee8be00
Al Elliott <adrelliott@gmail.com>
2023-08-02 11:56:09 +0200

    Add more icons

---
+++ resources/views/components/icon/cross.blade.php
@@ -0,0 +1,4 @@
+<path 
+    d="M6 18L18 6M6 6l12 12"
+    fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+/>
---
+++ resources/views/components/icon/uturn-right.blade.php
@@ -0,0 +1,4 @@
+<path 
+    d="M15 15l6-6m0 0l-6-6m6 6H9a6 6 0 000 12h3"
+    fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+/>
