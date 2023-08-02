6dff3a82596ec087b7ea89203b8f1aa79d883e1a
Al Elliott <adrelliott@gmail.com>
2023-08-02 12:05:39 +0200

    Tidy up components

---
+++ resources/views/components/static/3-benefits.blade.php
@@ -0,0 +1,20 @@
+<x-elements.3-blocks>
+    
+    <x-elements.3-blocks.block icon="briefcase">
+        <x-slot name="title">
+            Improve your Recruitment
+        </x-slot>
+    </x-elements.3-blocks.block>
+    
+    <x-elements.3-blocks.block icon="magnifier">
+        <x-slot name="title">
+            Increase your engagement
+        </x-slot>
+    </x-elements.3-blocks.block>
+    
+    <x-elements.3-blocks.block icon="users">
+        <x-slot name="title">
+            Empower your Managers
+        </x-slot>
+    </x-elements.3-blocks.block>
+</x-elements.3-blocks>
