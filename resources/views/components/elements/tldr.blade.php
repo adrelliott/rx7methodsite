80730f22d371e8c03a80f9191fe8bebed2d17ee9
Al Elliott <adrelliott@gmail.com>
2023-08-02 11:56:22 +0200

    Add TLDR component

--- resources/views/components/elements/tldr.blade.php
+++ resources/views/components/elements/tldr.blade.php
@@ -1,44 +1,62 @@
 <x-layouts.section-wide>
-    <x-elements.image-left>
-
-        <x-slot name="image" class="not-prose">
-            <h3 class=" font-black text-[200px]">
-                TL;DR
-            </h3>
-        </x-slot>
+    <x-elements.feature-block>
+        <x-slot name="subhead">TL;DR</x-slot>
+        <x-slot name="headline">At a glance...</x-slot>
+        <x-slot name="description">We get it. You're about 1/10th of a second away from pressing 'back', so let's give you the 9 second skinny.</x-slot>
         
-        <p class="lead">
-            We get it. You're about 1/10th of a second away from pressing 'back', so let's give you the 9 second skinny.
-        </p>
-        <dl>
-            <dt class="font-bold">Who's this for?</dt>
-            <dd>
+        <x-elements.feature-block.feature icon="users">
+            <x-slot name="title">
+                Who's this for?
+            </x-slot>
+            <x-slot name="description">
                 Business owners/leaders who have 20 - 200 employees
-            </dd>
-            <dt class="font-bold">What the heck is it?</dt>
-            <dd>
+            </x-slot>
+        </x-elements.feature-block.feature>
+            
+        <x-elements.feature-block.feature icon="questionmark">
+            <x-slot name="title">
+                What the heck is it?
+            </x-slot>
+            <x-slot name="description">
                 It's a model for workplace culture that consists of 7 foundations.
-            </dd>
-            <dt class="font-bold">Why only 7?</dt>
-            <dd>
-                ‘Cos our psychologists discovered that's all you need. 
-            </dd>
-            <dt class="font-bold">How does it work?</dt>
-            <dd>
+            </x-slot>
+        </x-elements.feature-block.feature>
+
+        <x-elements.feature-block.feature icon="lightbulb">
+            <x-slot name="title">
+                Why only 7?
+            </x-slot>
+            <x-slot name="description">
+                'Cos our psychologists discovered that's all you need. 
+            </x-slot>
+        </x-elements.feature-block.feature>
+
+        <x-elements.feature-block.feature icon="cog">
+            <x-slot name="title">
+                How does it work?
+            </x-slot>
+            <x-slot name="description">
                 We discover what you're missing and show you how to fix it.
-            </dd>
-            <dt class="font-bold">How can it help me?</dt>
-            <dd>
+            </x-slot>
+        </x-elements.feature-block.feature>
+
+        <x-elements.feature-block.feature icon="info">
+            <x-slot name="title">
+                How can it help me?
+            </x-slot>
+            <x-slot name="description">
                 Looking to grow, sell or raise investment? This is perfect for you.
-            </dd>
-            <dt class="font-bold">What do I do next?</dt>
-            <dd>
+            </x-slot>
+        </x-elements.feature-block.feature>
+
+        <x-elements.feature-block.feature icon="fastforward">
+            <x-slot name="title">
+                What do I do next?
+            </x-slot>
+            <x-slot name="description">
                 Take a look at the model - if it makes sense to you, let's talk.
-            </dd>
-            <dt class="font-bold">How much is it?</dt>
-            <dd>
-                We're not shy about our prices. Here's a calculator for you.
-            </dd>
-        </dl>
-    </x-elements.image-left>
+            </x-slot>
+        </x-elements.feature-block.feature>
+
+    </x-elements.feature-block>
 </x-layouts.section-wide>
