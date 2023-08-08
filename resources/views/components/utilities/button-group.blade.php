b4ec9b9a48dca2ac5047d2dfbed4f71948ff9b02
Al Elliott <adrelliott@gmail.com>
2023-08-08 12:34:34 +0200

    Add buttons & button group components

---
+++ resources/views/components/utilities/button-group.blade.php
@@ -0,0 +1,5 @@
+<div class="mx-auto">
+    <div class="flex w-full gap-2 sm:gap-4 justify-center">
+       {{ $slot }}
+    </div>
+</div>
--- resources/views/components/utilities/button.blade.php
+++ resources/views/components/utilities/button.blade.php
@@ -1,34 +1,20 @@
-@props(['color' => 'primary', 'size' => 'md', 'type' => 'button', 'href' => null, 'target' => null, 'ghost' => false, 'icon' => null])
-<a class="btn btn-primary sm:mb-0 w-full" href="{{ $href }}" target="{{ $target }}">
-    <div class="inline-flex items-center justify-center rounded-md border border-color-primary
+@props(['href' => null, 'target' => null, 'ghost' => false, 'icon' => null])
+<a 
+    class="
+        btn tracking-tight font-semibold lg:font-light py-2 px-4 rounded border border-primary
         @if($ghost) 
             bg-transparent text-primary hover:bg-primary hover:text-white
-        @else 
-            bg-primary text-white hover:bg-primary-dark 
+        @else
+            bg-primary text-white hover:bg-primary-dark hover:border-primary-dark  
         @endif
-        p-y-3.5 px-6
-        text-center text-base  
-    ">
-
-    @isset($icon)
-        <div class="mr-2">
-            {{ $icon }}
-        </div>
-    @endisset
-
-    {{ $slot }}
-
+    " 
+    href="{{ $href }}" 
+    target="{{ $target }}"
+>
+    <div class="inline-flex align-middle justify-center">
+        @isset($icon)
+            <x-icon :path="$icon" class="w-6 h-6 -mb-4 font-thin" strokeWidth="1" viewBox="0 0 28 28"/>    
+        @endisset
+        {{ $slot }}
     </div>
 </a>
-
-  
-{{--  
-  --tw-shadow: 0 4px 6px -1px rgb(0 0 0 / .1), 0 2px 4px -2px rgb(0 0 0 / .1);
-  --tw-shadow-colored: 0 4px 6px -1px var(--tw-shadow-color), 0 2px 4px -2px var(--tw-shadow-color);
-  box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000),var(--tw-ring-shadow, 0 0 #0000),var(--tw-shadow);
-  transition-property: color,background-color,border-color,text-decoration-color,fill,stroke,opacity,box-shadow,transform,filter,-webkit-backdrop-filter;
-  transition-property: color,background-color,border-color,text-decoration-color,fill,stroke,opacity,box-shadow,transform,filter,backdrop-filter;
-  transition-property: color,background-color,border-color,text-decoration-color,fill,stroke,opacity,box-shadow,transform,filter,backdrop-filter,-webkit-backdrop-filter;
-  transition-timing-function: cubic-bezier(.4,0,.2,1);
-  transition-duration: .2s;
-  transition-timing-function: cubic-bezier(.4,0,1,1); --}}
