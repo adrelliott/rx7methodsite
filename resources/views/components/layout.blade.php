5ab7f24073b05f7f837742b2ca3cf760ef7f1cf5
Al Elliott <adrelliott@gmail.com>
2023-08-08 16:49:51 +0200

    Update base styles

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,7 +16,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     </head>
-<body class="antialiased font-mona">
+<body class="antialiased tracking-tight">
 
     {{ $slot }}
     
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,48 +12,48 @@ export default {
   
   theme: {
     fontFamily: {
-      mona: ['Mona Sans', 'sans-serif'],
-      hubot: ['Hubot Sans', 'Tahoma', 'sans-serif'],
+      body: ['Mona Sans', 'Tahoma', 'sans-serif'],
+      heading: ['Hubot Sans', 'Tahoma', 'sans-serif'],
     },
-    fontSize: {
-      'sm': ['0.8rem', {
-        lineHeight: '1.12rem',
-        letterSpacing: '1em',
-        fontWeight: '400',
-      }],
-      'base': ['1rem', {
-        lineHeight: '1.4rem',
-        fontWeight: '300',
-      }],
-      'lg': ['1.1rem', {
-        lineHeight: '2.1324rem',
-        fontWeight: '400',
-      }],
-      'xl': ['1.6rem', {
-        lineHeight: '1.92rem',
-        fontWeight: '500',
-      }],
-      '2xl': ['1.777rem', {
-        lineHeight: '2.1324rem',
-        letterSpacing: '-0.01em',
-        fontWeight: '500',
-      }],
-      '3xl': ['2.369rem', {
-        lineHeight: '2.8428rem',
-        letterSpacing: '-0.01em',
-        fontWeight: '600',
-      }],
-      '4xl': ['3.157rem', {
-        lineHeight: '3.157rem',
-        letterSpacing: '-0.01em',
-        fontWeight: '600',
-      }],
-      '5xl': ['4.209rem', {
-        lineHeight: '4.209rem',
-        letterSpacing: '-0.01em',
-        fontWeight: '700',
-      }],
-    },
+    // fontSize: {
+    //   'sm': ['0.8rem', {
+    //     lineHeight: '1.12rem',
+    //     letterSpacing: '1em',
+    //     fontWeight: '400',
+    //   }],
+    //   'base': ['1rem', {
+    //     lineHeight: '1.4rem',
+    //     fontWeight: '300',
+    //   }],
+    //   'lg': ['1.1rem', {
+    //     lineHeight: '2.1324rem',
+    //     fontWeight: '400',
+    //   }],
+    //   'xl': ['1.6rem', {
+    //     lineHeight: '1.92rem',
+    //     fontWeight: '500',
+    //   }],
+    //   '2xl': ['1.777rem', {
+    //     lineHeight: '2.1324rem',
+    //     letterSpacing: '-0.01em',
+    //     fontWeight: '500',
+    //   }],
+    //   '3xl': ['2.369rem', {
+    //     lineHeight: '2.8428rem',
+    //     letterSpacing: '-0.01em',
+    //     fontWeight: '600',
+    //   }],
+    //   '4xl': ['3.157rem', {
+    //     lineHeight: '3.157rem',
+    //     letterSpacing: '-0.01em',
+    //     fontWeight: '600',
+    //   }],
+    //   '5xl': ['4.209rem', {
+    //     lineHeight: '4.209rem',
+    //     letterSpacing: '-0.01em',
+    //     fontWeight: '700',
+    //   }],
+    // },
     extend: {
       colors: {
           'primary': {
@@ -78,10 +78,10 @@ export default {
           },
           'highlight': '#E68F45',
       },
-      listStyleImage: {
-        checkmark: 'url("../assets/img/common/check.png")',
-        bolt: 'url("../assets/img/common/bolt.png")',
-      },
+      // listStyleImage: {
+      //   checkmark: 'url("../assets/img/common/check.png")',
+      //   bolt: 'url("../assets/img/common/bolt.png")',
+      // },
     },
   },
   plugins: [typography],
