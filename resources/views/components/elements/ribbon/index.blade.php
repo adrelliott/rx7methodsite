1c0437bdd82b3274d9a8a1ff33fce94b593708a6
Al Elliott <adrelliott@gmail.com>
2023-08-08 15:52:22 +0200

    wip

---
+++ resources/views/components/elements/ribbon/index.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.section-full-width class="bg-slate-100 py-4 ">
+    <div class="px-4 sm:px-6 max-w-6xl mx-auto">
+        {{ $slot }}
+    </div>
+</x-layouts.section-full-width>
--- resources/views/pages/astro/index.blade.php
+++ resources/views/pages/astro/index.blade.php
@@ -30,27 +30,32 @@
         
     </x-elements.heroes.centered>
 
-
-    <h1
-        class="font-bold font-heading leading-tighter mb-4 tracking-tighter dark:text-gray-200 md:text-6xl text-5xl">
-        Hero centered <span class="hidden lg:inline">create your website<br>with</span> <span
-            class="dark:text-white highlight text-accent">Astro 2.0</span> + Tailwind CSS</h1>
-            <p class="text-muted text-xl dark:text-slate-300 mb-6"><span class="hidden sm:inline"><span
-                class="font-semibold">AstroWind</span> is a free, customizable and production-ready
-            template for Astro 2.0 + Tailwind CSS.</span> <span
-            class="font-bold block mb-1 sm:hidden text-blue-600">AstroWind: Production-ready.</span>
-        Suitable for Startups, Small Business, Sass Websites, Professional Portfolios, Marketing
-        Websites, Landing Pages &amp; Blogs.</p>
-        <div class="mx-auto max-w-3xl">
-                
-            <div class="flex flex-col flex-nowrap gap-4 m-auto max-w-xs sm:flex-row sm:justify-center sm:max-w-md">
-                <div class="flex w-full sm:w-auto"><a class="w-full btn btn-primary sm:mb-0"
-                        href="https://github.com/onwidget/astrowind" rel="noopener" target="_blank"><svg
-                            class="h-5 w-5 -ml-1.5 mr-1" data-icon="tabler:download" height="1em"
-                            viewBox="0 0 24 24" width="1em">
-                            <use xlink:href="#ai:tabler:download"></use>
-                        </svg>&nbsp; Get template</a></div>
-                <div class="flex w-full sm:w-auto"><a class="w-full btn" href="#features">Learn more</a></div>
-            </div>
+    <x-layouts.section-full-width class="bg-slate-100 py-4 ">
+        <div class="px-4 sm:px-6 max-w-6xl mx-auto">
+        <div class="text-center">
+            <svg width="1em" height="1em" viewBox="0 0 24 24" class="w-4 h-4 inline-block align-text-bottom text-bold">
+                    <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                        stroke-width="2">
+                        <path d="M12 9h.01M3 5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5z" />
+                        <path d="M11 12h1v4h1"></path>
+                    </g>
+                </svg>
+            <span class="font-bold">Our Philosophy:</span> Simplicity, Best Practices and High Performance
         </div>
+    </x-elements.ribbon>
+
+    <div class="max-w-6xl mx-auto px-4 sm:px-6 py-4 text-md text-center font-medium">
+        <span class="font-bold">
+            <svg width="1em" height="1em" viewBox="0 0 24 24" class="w-5 h-5 inline-block align-text-top">
+                <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                    stroke-width="2">
+                    <path d="M12 9h.01M3 5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5z" />
+                    <path d="M11 12h1v4h1"></path>
+                </g>
+            </svg>
+            Philosophy:
+        </span> Simplicity, Best Practices and High Performance
+</div>
+
+
 </x-layouts.public>
