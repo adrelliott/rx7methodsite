bd99b769d5dbda1a469c3531bcf0d74c421be8ae
Al Elliott <adrelliott@gmail.com>
2023-08-08 13:44:04 +0200

    Refactor centered hero component

---
+++ resources/views/components/elements/heroes/centered.blade.php
@@ -0,0 +1,24 @@
+@props(['headline' => 'Define <x-slot name="headline"', 'subhead' => null, 'button' => null])
+<div class="w-full md:max-w-5xl lg:max-w-3xl xl:max-w-4xl mx-auto px-2 md:px-4 lg:px-6 xmb-10 xmd:mb-16 py-6 md:py-12 lg:py-20">
+    <div class="">
+        <div class="text-center">
+            
+            <h1 class="hero">
+                {{ $headline }}
+            </h1>
+
+            @isset($subhead)
+                <p class="text-muted text-lg mb-0">
+                    {{ $subhead }}
+                </p>
+            @endisset
+
+            {{ $slot }}
+
+            @isset($button)
+                {{ $button }}
+            @endisset
+
+        </div>
+    </div>
+</div>
