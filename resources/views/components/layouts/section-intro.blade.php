a0e6597a294d2782d8d46a5947df8ab50e24bfdc
Al Elliott <adrelliott@gmail.com>
2023-08-08 18:08:13 +0200

    Refactor section heading to use slots

--- resources/views/components/layouts/section-intro.blade.php
+++ resources/views/components/layouts/section-intro.blade.php
@@ -1,3 +1,16 @@
+@props(['subhead' => 'Set a slot named subhead', 'heading' => 'Set a slot name heading'])
 <div class="mb-8 md:mx-auto md:mb-12 text-center max-w-3xl">
-    {{ $slot }}
+
+    <p class="subhead">
+        {{ $subhead }}
+    </p>
+
+    <h2 class="text-center mb-6 mt-0">
+        {{ $heading }}
+    </h2>
+
+    <p class="lead text-center">
+        {{ $slot }}
+    </p>
+
 </div>
