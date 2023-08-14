061ef0cc60afcaf8aae4104d5014b404e9fe664f
Al Elliott <adrelliott@gmail.com>
2023-08-14 11:17:55 +0200

    Twaek 2-cols for md screens

--- resources/views/components/elements/columns/two-cols.blade.php
+++ resources/views/components/elements/columns/two-cols.blade.php
@@ -8,12 +8,12 @@
         '3/4:1/4' => ['basis-3/4', 'basis-1/4'],
     ]
 ])
-<div {{ $attributes->merge(['class' => 'flex flex-col md:flex-row ']) }}>
+<div {{ $attributes->merge(['class' => 'flex flex-col lg:flex-row ']) }}>
     
-    <div class="{{ $flexBasis[$colSplit][0] }} md:pr-6 lg:pr-10 xl:pr-12">
+    <div class="{{ $flexBasis[$colSplit][0] }} xmd:pr-6 lg:pr-10 xl:pr-12">
         {{ $column1 }}
     </div>
-    <div class="{{ $flexBasis[$colSplit][1] }} pt-4 md:pt-0 md:pl-6 lg:pl-10 xl:pl-12">
+    <div class="{{ $flexBasis[$colSplit][1] }} pt-4 lg:pt-0 md:pl-6 lg:pl-10 xl:pl-12">
         {{ $column2 }}
     </div>
 
